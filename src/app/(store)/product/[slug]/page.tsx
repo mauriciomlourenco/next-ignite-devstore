f8259c3e0348ec15ec0e93dec8cee8575dec87c0
Mauricio Machado Lourenço <mauricio.mchd@gmail.com>
2023-11-28 14:27:12 -0300

    Bug Product Page fixed, add to cart finished

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/components/add-to-cart-button'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 import { Metadata } from 'next'
@@ -35,7 +36,7 @@ export async function generateStaticParams() {
   const products: Product[] = await response.json()
   // return [{ slug: 'moleton-never-stop-learning' }]
 
-  products.map((product) => {
+  return products.map((product) => {
     return { slug: product.slug }
   })
 }
@@ -72,7 +73,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de
+            Em 12x s/ juros de{' '}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
@@ -121,12 +122,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
